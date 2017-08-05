5ec6662ff31ba40e8ab46652e289924b7e642568
Yorali <hideayeni@gmail.com>
2017-08-05 11:11:27 -0500

    Basic array made

--- server.js
+++ server.js
@@ -10,3 +10,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
+var reservations = [
+    {
+        name: "Nakamura",
+        mobile: 81-243-5263-5234,
+        email: "mushimushi@ww.com",
+        resID: 700
+    }, {
+        name: "Kasuga",
+        mobile: 81-343-5263-5234,
+        email: "mushimushi@ww.com",
+        resID: 701
+    }, {
+        name: "Mai",
+        mobile: 81-643-5263-5234,
+        email: "mushimushi@ww.com",
+        resID: 702
+    }, {
+        name: "Emi",
+        mobile: 81-443-5263-5234,
+        email: "mushimushi@ww.com",
+        resID: 703
+    }
+]
